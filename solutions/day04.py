fc74616eab300c2f16bc09cf2770c74cea30dcd0
payam.mesgari@gmail.com <payam.mesgari@gmail.com>
2023-12-05 09:14:56 +0100

    day04

---
+++ solutions/day04.py
@@ -0,0 +1,37 @@
+import re
+from cli import filepath
+
+with open(filepath) as inp:
+    lines = inp.read().splitlines()
+
+lines = [line.split('|') for line in lines]
+lines = [[line[0].split(':')[1].strip(), line[1].strip()] for line in lines]
+lines = [[re.split('\s+', line[0]), re.split('\s+', line[1])] for line in lines]
+
+cards = [line[0] for line in lines]
+wins = [line[1] for line in lines]
+
+def count_winning_numbers(card):
+    return len(list(filter(None, [n in wins[card] for n in cards[card]])))
+
+# print(count_winning_numbers(0))
+# print(count_winning_numbers(1))
+
+
+path = []
+def collect(card):
+    wining_numbers = count_winning_numbers(card)
+    if wining_numbers == 0:
+        path.append(card + 1)
+        return
+    
+    path.append(card + 1)
+    for i in range(1, wining_numbers + 1):
+        collect(card + i)
+
+for c in range(len(cards)):
+    collect(c)
+    
+print(len(path))
+# print(path)
+# print(collect(0))
